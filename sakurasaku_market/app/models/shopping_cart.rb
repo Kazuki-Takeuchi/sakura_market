fbdff0c08b0d29b4599b0565d63e30494212af2e
Kazuki Takeuchi <k.take57@gmail.com>
2018-03-16 01:09:29 +0900

    代金を表示できるようにした

--- sakurasaku_market/app/models/shopping_cart.rb
+++ sakurasaku_market/app/models/shopping_cart.rb
@@ -6,6 +6,12 @@ class ShoppingCart < ApplicationRecord
   enum payment_method_type: { 代引き: 1 }
   enum delivery_period_type: { "8-12":1, "12-14":2, "14-16":3, "16-18":4, "18-20":5, "20-21":6 }
 
+  # 代引き手数料 {上限,手数料}
+  @@cash_on_delivery_commission_table = { 10000 => 300, 30000 => 400, 100000 => 600, MAX: 1000 }
+  @@unit_shipping_fee = 600
+  @@unit_shipping_fee_count = 5
+  @@consumption_tax = 0.08
+
   def self.get_shopping_cart(user)
     shopping_cart = user.shopping_cart
     if shopping_cart.nil?
@@ -28,5 +34,27 @@ class ShoppingCart < ApplicationRecord
     }.select {|date|
         date.wday != 0 && date.wday != 6
       }.slice(2,13)
+  end
+
+  def self.get_cash_on_delivery_commission(subtotal)
+    @@cash_on_delivery_commission_table.keys.each {|border|
+      if border == :MAX
+        return @@cash_on_delivery_commission_table[:MAX]
+      elsif border > subtotal
+        return @@cash_on_delivery_commission_table[border]
+      end
+    }
+  end
+
+  def self.get_tax_included_price(price)
+    (price * (1 + @@consumption_tax)).floor
+  end
+
+  def get_shopping_cart_items_subtotal
+    self.shopping_cart_items.map{|item| item.commodity.price}.sum
+  end
+
+  def get_shipping_fee
+    ((self.shopping_cart_items.count + @@unit_shipping_fee_count - 1) / @@unit_shipping_fee_count) * @@unit_shipping_fee
   end
 end
