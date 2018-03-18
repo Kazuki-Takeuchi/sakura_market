f7af924c6dca5dac95cb9cd2fdc9de076e1219e1
Kazuki Takeuchi <k.take57@gmail.com>
2018-03-18 12:53:17 +0900

    ショッピングカートの商品validate追加

--- sakurasaku_market/app/models/shopping_cart_item.rb
+++ sakurasaku_market/app/models/shopping_cart_item.rb
@@ -1,4 +1,7 @@
 class ShoppingCartItem < ApplicationRecord
   belongs_to :shopping_cart
   belongs_to :commodity
+
+  validates_presence_of :shopping_cart_id
+  validates_presence_of :commodity_id
 end
